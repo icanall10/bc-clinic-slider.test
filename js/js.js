69feb994e5762390ca40c0958dbd0987557cad47
Marat Darbishev <marat.icanall@ya.ru>
2021-12-21 06:21:21 +0300

    Fixes

--- js/js.js
+++ js/js.js
@@ -13,14 +13,40 @@
                 dots: true
             });
 
+
         $('.twenty-slider .twenty')
-            .once()
+            .once('twenty')
             .twentytwenty({
                 default_offset_pct: 0.5, // How much of the before image is visible when the page loads
                 before_label: '',
                 after_label: '',
                 no_overlay: true,
                 click_to_move: false
+            });
+
+
+        $('.twenty')
+            .once('twenty', function () {
+                let $this = $(this);
+
+                let img = $this.find('img');
+
+                let count = img.length;
+                let loadedCount = 0;
+
+                img.on('load', function(){
+                    loadedCount++;
+
+                    if (loadedCount >= count) {
+                        $this.once('twenty-loaded').twentytwenty({
+                            default_offset_pct: 0.5,
+                            before_label: '',
+                            after_label: '',
+                            no_overlay: true,
+                            click_to_move: false
+                        });
+                    }
+                });
             });
 
     }
